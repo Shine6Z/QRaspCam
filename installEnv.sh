6a1894357bde23792ded06a46d3a1bacccc7e39c
bgallois <benjamin.gallois@upmc.fr>
2019-08-06 20:06:09 +0100

    [script] add dev env installation script

---
+++ installEnv.sh
@@ -0,0 +1,7 @@
+# Dev environment installation
+sudo apt-get install vim tmux git wget fonts-powerline 
+wget https://git.benjamin-gallois.fr/bgallois/vim/raw/master/install.sh
+chmod +x install.sh
+./install.sh
+rm install.sh
+
