aaf5b62ceda969f26e99770c391d1f7747cf7e06
bgallois <benjamin.gallois@upmc.fr>
2019-08-11 18:20:20 +0100

    [script] update client installation

--- client/install.sh
+++ client/install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y qt5-default cmake
+sudo apt-get install -y qt5-default cmake patchelf
 git clone https://github.com/cedricve/raspicam
 cd raspicam
 mkdir build
@@ -9,3 +9,8 @@ sudo make install
 sudo ldconfig
 cd ..
 rm -rf raspicam
+git clone https://github.com/bgallois/FDS2019.git
+cd client/
+qmake client.pro
+make
+sudo cp client /usr/local/bin/
